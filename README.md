18dd25a8315950717f0fc61366fedfa1f38d24e3
krizhnaa <krizhnaprem@gmail.com>
2023-11-13 19:48:14 +0530

    Animating the snake's body

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 # <1> Creating a turtle and screen function and change bg to black.
 # <2> Creating snake's body.
+# <3> Animating the movement of snake's body
--- main.py
+++ main.py
@@ -7,14 +7,19 @@ screen.bgcolor('black')
 screen.title('Snake Game')
 
 positions = [(0,0), (-20, 0), (-40, 0)]
+segments = []
+is_game_on = True
 
 for pos in positions:
     snake = Turtle('square')
     snake.color('white')
     snake.penup()
     snake.goto(pos)
+    segments.append(snake)
 
-
+while is_game_on:
+    for seg in segments:
+        seg.forward(20)
 
 
 
