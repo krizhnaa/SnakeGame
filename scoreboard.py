e1d0fc7a4ff838269e7019c9b25f9d39b9cc2858
krizhnaa <krizhnaprem@gmail.com>
2023-11-15 12:17:07 +0530

    Cleaner code

--- scoreboard.py
+++ scoreboard.py
@@ -9,10 +9,13 @@ class Scoreboard(Turtle):
         self.penup()
         self.color('white')
         self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
         self.write(f'Scoreboard : {self.score}', align='center', font=('Arial', 15, 'normal'))
 
     def scored(self):
         self.score += 1
         self.clear()
-        self.write(f'Scoreboard : {self.score}', align='center', font=('Arial', 15, 'normal'))
+        self.update_scoreboard()
 
